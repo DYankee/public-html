08cbba2ae494530db89173f25a0ce8725bbfc24d
Geary <z.geary@clasnet.sunyocc.edu>
2022-12-01 16:19:22 -0500

    started building basic site structure

---
+++ js/Dota2.js
@@ -0,0 +1,10 @@
+const dotaImgs =[
+    "../img/void-spirit-dota-2_1920x1080.jpg",
+    "../img/Optimized-Dota-2-by-Valve.jpg",
+]
+
+document.getElementById("img1").addEventListener("click", nextImg);
+
+function nextImg(){
+    alert("test");
+}
